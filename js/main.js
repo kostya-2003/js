adc7d9a501ed98cf2d4296f3f188c427f8fe5f2a
Kostya Haxverdyan <kkkk.hhhh.03@mail.ru>
2022-07-22 18:32:19 +0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,5 @@
+"use strict";
+
+const headerImg = document.querySelector("header img").src="/img/bg2.jpg";
+const promo = document.querySelector(".main_promo").remove();
+const title = document.querySelector("title").innerText = "Hitman's Wife's Bodyguard (2021 Movie)";
